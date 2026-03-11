57ed09c07b78b3ddb8b30eed7c673b68afd9532f
onkar45 <onkarrede45@gmail.com>
2026-03-11 11:37:54 +0530

    sfas

--- app/database.py
+++ app/database.py
@@ -23,3 +23,27 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+CREATE USER 'bookinguser'@'localhost' IDENTIFIED BY 'MahitNahi@12';
+GRANT ALL PRIVILEGES ON slot_booking_db.* TO 'bookinguser'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+
+
+
+ssh root@192.46.213.87
+cd ~/slot-booking-backend
+
+
+0af52eb334a03a2c.vercel-dns-017.com.source venv/bin/activate
